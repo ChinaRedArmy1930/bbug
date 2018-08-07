acf3b15b188dcaf9d181262ea5c8874526b98024
ChinaRedArmy1930 <syyxy@outlook.com>
2018-08-07 19:52:35 +0800

    RecursiveGetUrl TODO

--- bug.py
+++ bug.py
@@ -3,18 +3,34 @@ import urllib
 import re
 
 def getpage(url):
-	return urllib.urlopen(url)
+	return urllib.urlopen(url).read();
 
-def geturlformpage(content):
-	pattern=re.compile(r'^http.*\.(gz|pdf|.ico)$');
-	allurl=re.findall(r'.*?\"(https://.*?)\"',content,re.M|re.X|re.I);
+def getallurlformpage(content):
+	allurl=re.findall(r'.*?\"(https://.*?)\"',content.replace('\n','').replace('\t','').replace(' ',''),re.M|re.X|re.I);
+	return  allurl
 	#allurl[:]是 allurl 的一个内存拷贝
-	for i in allurl[:]:
+	
+def getcurrenturl(url):
+	url_temp=url;
+	pattern=re.compile(r'^http.*\.(gz|pdf|.ico)$');
+	for i in url_temp[:]:
 		if re.match(pattern,i.strip()):
-			allurl.remove(i)
-	return allurl
-if __name__ == '__main__':
-	allpage=getpage("https://gcc.gnu.org/onlinedocs/").read();
-	for i in geturlformpage(allpage.replace('\n','').replace('\t','').replace(' ','')):
+			url_temp.remove(i)
+	return url_temp
+	
+def Geturl(url):
+	allpage=getpage(url);
+	allurl=getallurlformpage(allpage);
+	currenturl=getcurrenturl(allurl);
+	return currenturl;
+
+def RecursiveGetUrl(url):
+	url=Geturl(url);
+	for i in url:
 		print i
+		if i:
+			RecursiveGetUrl(url)
+	
+if __name__ == '__main__':
+	RecursiveGetUrl("https://gcc.gnu.org/onlinedocs/")
 	
