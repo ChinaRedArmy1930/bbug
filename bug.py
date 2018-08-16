7f365efaf56e66dd97ee979252f607a76bbf143b
ChinaRedArmy1930 <syyxy@outlook.com>
2018-08-16 20:23:54 +0800

    modify 1) 使用requests代替urlopen 添加url-header 2)修改正则表达式, 使用python (?! )代替[^()] 3)修改部分bug

--- bug.py
+++ bug.py
@@ -3,6 +3,7 @@ import urllib
 import random 
 import string
 import re
+import requests
 
 class EachPage:
 	urlList = [];
@@ -33,23 +34,25 @@ class EachPage:
 		self.url=url;
 
 
-
+UrlSet = set()
 head = EachPage("https://gcc.gnu.org/onlinedocs/");
-	
+deeplength=0
 
 def getpage(url):
-	fd = urllib.urlopen(url);
-	if fd.getcode() == 200:
-		return fd.read()
+	header={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'};# chrome header
+	req = requests.get(url,headers=header,verify=False);
+	if req.status_code == 200:
+		return req.text
 	else:
-		print '\033[1m' + url + " requst failed!"+'\033[0m'
+		print '\033[1m' + url +','+str(fd.getcode())+','+'requst failed!'+'\033[0m'
 		return None
+
 		
 def getallurlformpage(content):
 	if not content:
 		return None;
 	allurl=re.findall(r'.*?(href=\".*?\")',content.replace('\n','').replace('\t','').replace(' ',''),re.M|re.X|re.I);
-	print allurl
+	#print allurl
 	return  allurl
 	
 	
@@ -57,23 +60,28 @@ def getcurrenturl(url):
 	if not url:
 		return None;
 	url_temp=url;
-	pattern_del=re.compile(r'^href=.*\.(gz|pdf|ico|css)$');
-	pattern_http=re.compile(r'^href=\"(http(s?):.*)[^(gz|pdf|ico|css)]\"');
-	pattern_join=re.compile(r'^href=\"([^(http|mailto)]).*\"');
+	pattern_del=re.compile(r'^(href=)\".*\.(gz|pdf|ico|css)\"$');
+	pattern_http=re.compile(r'^(href=)\"http(s?):.*?(?!(gz|pdf|ico|css))\"');
+	pattern_join=re.compile(r'^(href=)\"(?!(http|mailto)).*?(?!(gz|pdf|ico|css))\"');
 	#allurl[:]是 allurl 的一个内存拷贝
 	for i in url_temp[:]:
-		print i
+		#print i
 		url_temp.remove(i)
 		if re.match(pattern_del,i.strip()):
-			print "del :"+i
+			#print "del :"+i
 			continue;
 		if re.match(pattern_http,i.strip()):
 			#去掉 herf:"  和 "
-			url_temp.append(i[6:-1])
+			#print "http :"+i
+			if i[6:-1] not in url_temp:
+				url_temp.append(i[6:-1])
+				UrlSet.add(i[6:-1])
 			continue;
 		if re.match(pattern_join,i.strip()):
-			print "join :"+i
-			url_temp.append(temppage.getLocalUrl()+i[6:-1])
+			#print "join :"+i
+			if temppage.getLocalUrl()+i[6:-1] not in url_temp:
+				url_temp.append(temppage.getLocalUrl()+i[6:-1])
+				UrlSet.add(temppage.getLocalUrl()+i[6:-1])
 			continue;
 	return url_temp
 	
@@ -90,22 +98,25 @@ def Geturl(url):
 		pass
 	return currenturl;
 
-def RecursiveGetUrl(url):
-	if not head.getLocalUrl():
-		head.setLocalUrl(url);
-	print "==============================="
-	print "headurl :"+url
-	print "this page have this urls:"
+def RecursiveGetUrl(url,number):
+	UrlSet.add(url)
+	print "number of UrlSet = %d"%len(UrlSet)
+	if len(UrlSet) > number:
+		return;
 	url=Geturl(url);
 	if not url:
 		return None
 	for i in url:
-		print i 
+		#print i 
 		if i:
-			RecursiveGetUrl(i)
+			RecursiveGetUrl(i,number)
 			pass
 	
 
 if __name__ == '__main__':
-	RecursiveGetUrl("https://gcc.gnu.org/onlinedocs/gcc-8.2.0/gcc/Machine-Constraints.html#Machine-Constraints")
-	
+	#递归遍历起始url找500个页面
+	RecursiveGetUrl("https://gcc.gnu.org/onlinedocs/gcc-8.2.0/gcc/DEC-Alpha-Options.html#DEC-Alpha-Options",500)
+	with open('url.txt','w') as urlFile:
+		for i in UrlSet:
+			urlFile.writelines(i+"\n");
+		
